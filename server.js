5121474335e90efc12a33f0fccbe1604cd8837b5
Jianingzhang235 <jianing_chang@hotmail.com>
2019-05-31 04:46:11 -0700

    finished server and it is functional

--- server.js
+++ server.js
@@ -3,39 +3,25 @@ const morgan = require('morgan');
 const bodyParser = require('body-parser');
 const db = require('./database/index.js');
 
-
 var app = express();
-var port = 3004;
-
-app.use(function (req, res, next) {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header('Access-Control-Allow-Methods', 'PUT, GET, POST, DELETE, OPTIONS');
-  res.header("Access-Control-Allow-Headers", "X-Requested-With");
-  res.header('Access-Control-Allow-Headers', 'Content-Type');
-  next();
-});
-
+var port = 3005;
 
 app.use(morgan('tiny'));
 app.use(bodyParser.json());
 app.use(express.static('public'));
-//TODO
-app.get('/reviews', (req, res) => {
+
+app.get('/items', (req, res) => {
   db.fetch((err, results) => {
+    console.log('typeof', typeof results);
+    console.log(results);
     res.status(200).send(JSON.stringify(results)).end();
   });
 });
 
-app.use(function (req, res, next) {
-  res.status(404).send("Sorry can't find that!");
-});
-//
-var server = app.listen(port, (err) => {
+app.listen(port, (err) => {
   if (err) {
     console.log(err);
     return;
   }
-  console.log(`Listening on port ${port}: `);
+  console.log(`Listening on port ${port}:`);
 });
-
-// module.exports = server;
