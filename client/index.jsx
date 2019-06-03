9060ff93ac4cf2feb3492dcba81b3daac8afec8c
Jianingzhang235 <jianing_chang@hotmail.com>
2019-06-03 01:26:28 -0700

    it works again but the css can not render into the server

--- client/index.jsx
+++ client/index.jsx
@@ -4,6 +4,20 @@ import ReviewList from './ReviewList.jsx';
 // import More from './More.jsx';
 import axios from 'axios';
 
+const host = location.hostname || 'localhost';
+
+const otherItemsStyle = {
+  fontFamily: "Graphik Webfont,-apple-system,BlinkMacSystemFont,Roboto,Droid Sans,Segoe UI,Helvetica,Arial,sans-serif"
+};
+
+const jnz_title_style = {
+  color: '#212121',
+  textAlign: 'left',
+  paddingTop: '30px',
+  fontSize: '20px',
+  fontWeight: 'bold'
+};
+
 class App extends React.Component {
   constructor(props) {
     super(props);
@@ -20,20 +34,20 @@ class App extends React.Component {
     this.fetchItems();
   }
 
-  fetchItems(next) {
-    axios.get('http://localhost:3005/items')
-      .then((res) => {
-        this.setState({
-          otherItems: res.data
-        });
-        if (next) {
-          next();
-        }
-      })
-      .catch((err) => {
-        console.log(err);
+fetchItems(next) {
+  axios.get('http://localhost:3005/items')
+    .then((res) => {
+      this.setState({
+        otherItems: res.data
       });
-  }
+      if (next) {
+        next();
+      }
+    })
+    .catch((err) => {
+      console.log(err);
+    });
+}
 
   render() {
     return (
