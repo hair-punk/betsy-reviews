0430d98926e9bcec3558a89d0f3a4b7c70ad6131
Jianingzhang235 <jianing_chang@hotmail.com>
2019-06-03 06:07:44 -0700

    put more css style

--- client/Item.jsx
+++ client/Item.jsx
@@ -1,22 +1,100 @@
 import React from 'react';
 var moment = require('moment');
 
+const jnz_profile_style = {
+  width: 50,
+  height: 50,
+  borderWidth: 1,
+  backgroundColor: '#E1E3DF',
+  borderColor: 'rgba(0, 0, 0, 0.05)',
+  borderRadius: 25,
+  overflow: 'hidden',
+  display: 'inline-block'
+}
+const jnz_review_style = {
+  marginTop: 0,
+  color: '#222 !important',
+  verticalAlign: 'top',
+  display: 'inline-block'
+}
+const jnz_reviewInfo_style = {
+  display: 'inline-block'
+}
+const jnz_reviewName_style = {
+  textDecoration: 'underline',
+  marginRight: 6,
+  display: 'inline-block'
+}
+const jnz_reviewTime_style = {
+  marginLeft: 6,
+  display: 'inline-block'
+}
+
+const jnz_reviewStar_style = {
+  listStyle: 'none',
+  marginTop: 10,
+  marginBottom: 0,
+  verticalAlign: 'bottom',
+}
+const jnz_content_style = {
+  margin: '0, 0, 12',
+  paddingLeft: 84,
+  fontSize: 14,
+  lineHeight: 1.71429,
+  color: '#222'
+}
+// const jnz_uploadedPic_style= {
+//     display: 'block',
+//     margin: 0,
+//     padding: 0,
+//     border: 0,
+//     width: 300,
+//     height: 300
+
+// }
+
+const jnz_productUrl_style = {
+  display: 'inline-block',
+}
+const jnz_productPic_style = {
+  display: 'inline-block',
+  backgroundColor: '#92877C',
+  borderColor: 'rgba(0, 0, 0, 0.05)',
+ boxSizing: 'border-box',
+  margin: 84,
+  padding: 0,
+  border: 0,
+  width: 50,
+  height: 50
+}
+
+// const jnz_url_style = {
+//   display: 'inline-block',
+//   verticalAlign: 'middle'
+// }
+const jnz_proUrl_style = {
+  display: 'inline-block',
+  marginTop: 100,
+  verticalAlign: 'top'
+}
 var Item = ({item}) => (
   <div className="jnz-item">
-    <img src={item.userProfile} className="jnz-profile"/>
-    <span className="jnz-itemTitle">{item.username}</span>
-    {/* {moment().format("MMM Do YY")} */}
-    {/* <span className="jnz-reviewDate">{.format("MMM Do YY")}</span> */}
-    <span className = "jnz-reviewDate">{moment(item.reviewDate.slice(0, 10)).format("MMM Do YY")}</span>
-    <div className="jnz-stars">{item.stars}</div>
-    <div className="jnz-content">{item.content}</div>
-    <img src={item.uploadPics} className="jnz-upload"/>
-
-    <a href="">
-      <img src={item.imageUrl} className="jnz-image"/>
-    </a>
-    <a className="jnz-proUrl" href="">{item.productURL}</a>
-
+    <img src={item.userProfile} className="jnz-profile" roundAsCircle={true} style={jnz_profile_style}/>
+    <ul style={jnz_review_style}>
+      <li className = "jnz-reviewInfo" style={jnz_reviewInfo_style}>
+        <span className="jnz-itemTitle" style={jnz_reviewName_style}>{item.username}</span>
+        <span className = "jnz-reviewDate" style={jnz_reviewTime_style}>{moment(item.reviewDate.slice(0, 10)).format("MMM Do YY")}</span>
+      </li>
+      <li className="jnz-stars" style={jnz_reviewStar_style}>{item.stars}</li>
+    </ul>
+    <div className="jnz-content" style={jnz_content_style}>{item.content}</div>
+    {/* <img className="jnz-upload" src={item.uploadPics}  style={jnz_uploadedPic_style}/> */}
+    <div style={jnz_productUrl_style}>
+      <a href="">
+        <img src={item.imageUrl} className="jnz-image" style={jnz_productPic_style}/>
+      </a>
+      <a className="jnz-proUrl" href="" style={jnz_proUrl_style}>{item.productURL}</a>
+    </div>
   </div>
 )
 
--- client/index.jsx
+++ client/index.jsx
@@ -6,17 +6,61 @@ import axios from 'axios';
 
 const host = location.hostname || 'localhost';
 
-const otherItemsStyle = {
+const allItemsStyle = {
   fontFamily: "Graphik Webfont,-apple-system,BlinkMacSystemFont,Roboto,Droid Sans,Segoe UI,Helvetica,Arial,sans-serif"
 };
 
 const jnz_title_style = {
   color: '#212121',
+  paddingLeft: '36px',
   textAlign: 'left',
   paddingTop: '30px',
   fontSize: '20px',
   fontWeight: 'bold'
 };
+
+const jnz_reviews_star_style = {
+  marginLeft: 10
+}
+const jnz_reviews_num = {
+  marginLeft: 6
+}
+const jnz_reviews_container = {
+    fontSize: '14px',
+    color: '#212121',
+    textAlign: 'left',
+    maxWidth: '1272px',
+    minWidth: '972px',
+    paddingLeft: '36px',
+    paddingRight: '36px',
+    justifyContent: 'space-around',
+    marginBlockStart: '1em',
+    marginBlockEnd: '1em',
+    marginInlineStart: '0px',
+    marginInlineEnd: '0px',
+    paddingInlineStart: '40px'
+  }
+const jnz_readAll_style = {
+  color: '#FFF',
+  textDecoration: 'none',
+  backgroundColor: '#222222',
+  borderRadius: 3,
+  borderStyle: 'solid',
+  borderWidth: 1,
+  display: 'inline - block',
+  fontFamily: 'inherit',
+  fontSize: 14,
+  fontWeight: 'bold',
+  height: 38,
+  lineHeight: 1.4,
+  padding: '8 12',
+  textAlign: 'center',
+  userSelect: 'none',
+  whiteSpace: 'nowrap',
+  webkitTapHighlightColor: 'transparent',
+  webkitAppearance: 'none'
+}
+
 
 class App extends React.Component {
   constructor(props) {
@@ -51,19 +95,24 @@ fetchItems(next) {
 
   render() {
     return (
-      <div id="reviews">
-        <h1 className="jnz-title">Reviews</h1>
-        <ReviewList items={this.state.otherItems} handleClick={this.clickItem.bind(this)}/>
-        <div className="jnz-more">
-          <button className="jnz-add16More">+ More</button>
+      <div style={allItemsStyle}>
+        <div id="reviews">
+          <span className="jnz-title" style={jnz_title_style}>Reviews</span>
+          <span className="jnz-title" style={jnz_reviews_star_style}>★★★★☆</span>
+          <span style={jnz_reviews_num}>(1,371)</span>
         </div>
-        <div className="jnz-readAll">
-          <button className="jnz-readAllReviews">Read All Reviews
-          + (this.list.length)}</button>
-        </div>
-         <div className="jnz-reviewPics">
-           Photos from reviews
-           ReviewList.map.pics
+        <div className="jnz-reviews-container" style={jnz_reviews_container}>
+          <ReviewList items={this.state.otherItems} handleClick={this.clickItem.bind(this)}/>
+          <div className="jnz-more" >
+            <button className="jnz-add16More">+ More</button>
+          </div>
+          <div className="jnz-readAll">
+            <button className="jnz-readAllReviews" style={jnz_readAll_style}>Read All Reviews</button>
+          </div>
+          <div className="jnz-reviewPics">
+            Photos from reviews
+            ReviewList.map.pics
+          </div>
         </div>
       </div>
     );
